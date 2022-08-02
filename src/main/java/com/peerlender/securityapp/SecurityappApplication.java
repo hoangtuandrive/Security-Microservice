4de7559eee5e40560dd39843b0456cff48e8687e
Hoàng Tuấn <80930248+hoangtuandrive@users.noreply.github.com>
2022-08-02 19:20:46 +0700

    Add User Controller

--- src/main/java/com/peerlender/securityapp/SecurityappApplication.java
+++ src/main/java/com/peerlender/securityapp/SecurityappApplication.java
@@ -1,13 +1,24 @@
 package com.peerlender.securityapp;
 
+import com.peerlender.securityapp.user.model.User;
+import com.peerlender.securityapp.user.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityappApplication {
+public class SecurityappApplication implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityappApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(new User("tuan1","123"));
+		userRepository.save(new User("tuan2","124"));
+	}
 }
---
+++ src/main/java/com/peerlender/securityapp/application/UserController.java
@@ -0,0 +1,25 @@
+package com.peerlender.securityapp.application;
+
+import com.peerlender.securityapp.user.exception.UserNotFoundException;
+import com.peerlender.securityapp.user.model.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user") //add /user to endpoint path
+public class UserController {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping("/validate")
+    public String validateTokenAndGetUsername(@RequestHeader("Authorization") String token){
+        return userRepository.findById(token).orElseThrow(UserNotFoundException::new).getUsername();
+    }
+}
